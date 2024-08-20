1d4aaec88c65cd616ced344604e6c3e9802fa285
BocharnikovaMasha <bocarnikovamasa@gmail.com>
2024-08-20 16:00:06 +0300

    seven

--- online/urls.py
+++ online/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('catalog/', catalog, name='catalog'),
     path('orders/', orders, name='orders'),
-    path('order_create/', order_create, name='order_create'),
+    path('order_create/<int:product_id>', order_create, name='order_create'),
     path('details/<int:product_id>', details, name='details'),
 ]
--- online/views.py
+++ online/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from .models import Order
 
@@ -23,7 +23,12 @@ def orders(request):
           print(order.creаted)
      return render(request, 'online/orders.html', {'posts': archive})
 
-def order_create(request):
+def order_create(request, product_id):
+     product = Product.objects.get(id=product_id) 
+     if request.method == 'POST':
+          adress=request.POST['adress']
+          Order.objects.create(adress=adress, product=product)
+          return redirect('orders')
      return render(request, 'online/order_create.html')
 
 def details(request, product_id):
